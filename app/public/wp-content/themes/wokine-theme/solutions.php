3e7fd4c358bc14df29d96a4444de7033ce17558d
Greg <gregoire.duhem@epitech.eu>
2026-02-26 17:22:49 +0100

    adding first par of solutions section

--- app/public/wp-content/themes/wokine-theme/solutions.php
+++ app/public/wp-content/themes/wokine-theme/solutions.php
@@ -1,6 +1,17 @@
 <section class="solutions">
-    <div class="solutions-content">
-        <img src="" alt="">
-        <h2 class="solutions-title">Des solutions techniques complètes pour tous vos projets</h2>
+  <div class="solutions-image-container">
+    <img class="solutions-image" src="/wp-content/themes/wokine-theme/frontend/src/assets/images/solutions-bg.png" alt="Solutions bg">
+  </div>
+  <div class="solutions-content">
+    <h3 class="solutions-title text-gradient">Des solutions techniques complètes pour tous vos projets</h3>
+    <div class="card-container">
+      <div class="card-1">
+        <div class="card-1-content">
+          <div class="card-1-title">Aenean aliquam</div>
+          <div class="card-1-text">Imperdiet faucibus egestas ipsum et mattis. Duis non et justo vestibulum vitae. Tortor tincidunt felis neque congue. In et tempus dignissim ullamcorper eget pellentesque vestibulum pellentesque. Viverra eget elit id eget. Ut cursus sed et massa morbi feugiat. Ac faucibus ante congue nisi a amet elit est. Fermentum.</div>
+        </div>
+        <a class="card-1-button">Voir plus</a>
+      </div>
     </div>
+  </div>
 </section>
